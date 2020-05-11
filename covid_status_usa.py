b6d5be4fa67c2d5c39f2b9e934abc7c254faa513
Emmett Stewart <cafeninja@gmail.com>
2020-05-12 00:54:20 +0200

    US covid file.  Can use any country as an variable to be set in the file.  Added also active cases math.

--- covid_status_usa.py
+++ covid_status_usa.py
@@ -2,24 +2,26 @@
 import urllib.request  # instead of urllib2 like in Python 2.7
 import json
 
-
 urlData = "https://pomber.github.io/covid19/timeseries.json"
-
+country = "US"
+# county names = Malta, Italy, US, United Kingdom
 def printResults(data):
     # Use the json module to load the string data into a dictionary
     theJSON = json.loads(data)
 
-    datakeys= theJSON.keys()
-    mtdata= list(theJSON["Malta"])
+    datakeys = theJSON.keys()
+    mtdata = list(theJSON[country])
     # print(mtdata)
-    
-    print("Covid stats for Malta (last for days)")
-    print(f"Date:\t\tConfirmed:\t\tDeaths:\t\tRecoveries:")
+
+    print(f'Covid stats for {country} (last four days)')
+    print(f"Date:\t\tConfirmed:\t\tDeaths:\t\t\tRecoveries:")
     last_for = [-4, -3, -2, -1]
     for i in last_for:
-    	fmt_data= mtdata[i]
-    	print(f"{fmt_data['date']}\t\t{fmt_data['confirmed']}\t\t\t{fmt_data['deaths']}\t\t\t{fmt_data['recovered']}")
-    	
+        fmt_data = mtdata[i]
+        print(f"{fmt_data['date']}\t\t\t{format(fmt_data['confirmed'], ',d')}\t\t\t{format(fmt_data['deaths'], ',d')}\t\t\t{format(fmt_data['recovered'], ',d')}")
+    activecases = 0
+    activecases = format((mtdata[-1]['confirmed']) - (mtdata[-1]['deaths']) - (mtdata[-1]['recovered']), ',d')
+    print(f"Total number of active cases: {activecases}")
 
 
 # Open the URL and read the data
