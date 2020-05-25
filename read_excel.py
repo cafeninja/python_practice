01a61ebd76cde2bf1583d25eb674f83dfa9757b0
CafeNinja <cafeninja@gmail.com>
2020-05-25 09:16:11 +0200

    It is all working.
    Output appending to existing excel file. no major errors on comment alteration. Will handover to Terence 29 May 2020.

--- read_excel.py
+++ read_excel.py
@@ -14,9 +14,42 @@
 #
 import re
 import string
+import datetime
+import dateutil.relativedelta
+import contextlib
+import os
 from datetime import datetime
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
+
+# Find OT file.  If there, delete... then create a tnew one.202004_NOC OT_reporting.xlsx
+now = datetime.now()
+fnlastmonth = (now + dateutil.relativedelta.relativedelta(months=-1)).strftime("%Y%m")
+filename = fnlastmonth + '_NOC_OT_reporting.xlsx'
+#print(filename)
+
+with contextlib.suppress(FileNotFoundError):
+    os.remove(filename)
+book = Workbook()
+sheet = book.active
+titlerow = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1']
+columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+sheet['A1'] = 'Employee Code'
+sheet['B1'] = 'Name of Employee'
+sheet['C1'] = 'Surname of Employee'
+sheet['D1'] = 'Date of Overtime'
+sheet['E1'] = 'Overtime Code'
+sheet['F1'] = 'Hours'
+sheet['G1'] = 'Reason'
+for cell in titlerow:
+    sheet[cell].fill = PatternFill(start_color='FFC000', end_color='FFC000', fill_type='solid')
+    sheet[cell].font = Font(bold=True)
+for column in columns:
+    sheet.column_dimensions[column].width = 22
+book.save(filename)
+
 
 workbook = load_workbook('2020_Malta_NOC_Schedule.xlsx')
 first_sheet = workbook.sheetnames[1]
@@ -77,8 +110,10 @@ for row in worksheet.iter_rows():
                 #print(namelist[1])
             elif len(namelist) <= 1:
                 note = str(re.sub('[\n\'\[\]!@#$]', '', commentRaw))
-                note = "Identified Holiday: " + note
+                #note = "Identified Holiday: " + note
+                fmtrow.append("Identified Holiday:")
                 fmtrow.append(note)
+                fmtrow.append('.')
                 holidaylist.append(cell.column)
                 #print(note)
             # Add Date to list for output.
@@ -102,13 +137,21 @@ for row in worksheet.iter_rows():
                 #print(hrs)
                 fmtrow.append(ottime[1])
                 #print(ottime[1])
-            # show collected output whic will become the feed for new XLXS
+            wb = load_workbook(filename)
+            ws = wb.worksheets[0]
+            ws.append(fmtrow)
+            wb.save(filename)
+#========================
             print(fmtrow, sep=" | ")
 
 # Format of output needed for HR.
 # Employee Code	| Name of Employee  | Surname   | Date          | Overtime Code	| Hours
 # 123	        | Nadine	    | Dalli	| 2/8/2017	| OT15	        | 3
-
+hebreak = ['----Holidy Entries--------', '----Holidy Entries--------']
+wb = load_workbook(filename)
+ws = wb.worksheets[0]
+ws.append(hebreak)
+wb.save(filename)
 print('----Holidy Entries--------')
 #print(holidaylist)   # for troubleshooting
 for i in holidaylist:
@@ -157,15 +200,21 @@ for i in holidaylist:
                     fmtrow.append(cal)
                     fmtrow.append('OT20')
                     fmtrow.append('12')
+                    wb = load_workbook(filename)
+                    ws = wb.worksheets[0]
+                    ws.append(fmtrow)
+                    wb.save(filename)
                     print(fmtrow, sep=" | ")
 
 ## Add list of persons working on feast which is not on Saturday or Sunday
+sbreak = ['----Scheduled off Weekday Feast--------', '----Scheduled off Weekday Feast--------']
+wb = load_workbook(filename)
+ws = wb.worksheets[0]
+ws.append(sbreak)
+wb.save(filename)
 print('----Scheduled off Weekday Feast--------')
 #print(holidaylist)   # for troubleshooting
 for i in holidaylist:
-    # print(i)   # for troubleshooting
-    #holidaycol = str(i+"3:"+i+"15")
-    # print(holidaycol)
     for row in worksheet.iter_rows():
         for cell in row:
             if cell.column == i:
@@ -214,5 +263,9 @@ for i in holidaylist:
                     # Add Date to list for output.
                     fmtrow[2] = cal
                     if len(fmtrow) == 4 and len(fmtrow[0]) > 2:
+                        wb = load_workbook(filename)
+                        ws = wb.worksheets[0]
+                        ws.append(fmtrow)
+                        wb.save(filename)
                         print(fmtrow, sep=" | ")
 ##  Make xlxs and input each row into it.
